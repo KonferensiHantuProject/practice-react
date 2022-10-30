5b227ed65b7ce4ddda7537b1868ae625f5688b5e
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-10-30 21:08:07 +0700

    Adding New Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import './App.css';
+import Navbar from './Navber';
+import Home from './Home';
 
 function App() {
 
-  const title = 'Komponen App';
-  const likes = 200;
-  // const person = { nama: 'Toni', umur: 21};
-  const url = 'http://www.google.com';
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Jumlah Suka { likes }</p>
-
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ 'Test' }</p>
-        <p>{ [1,2,3,4,5,6] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={ url }>Google</a>
+          <Home/>
       </div>
     </div>
   );
---
+++ src/Navber.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Blog Bone</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Blog Baru</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
