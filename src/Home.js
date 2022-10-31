d7e1ee59377c77716bd717bdf77c50138b7654bc
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-10-31 14:58:35 +0700

    Reusing a component

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,15 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState([
-        { title: 'Web Baru ku', body: 'Lorem Ipsum', author: 'mario', id: 1 },
+        { title: 'Web Baru ku', body: 'Lorem Ipsum', author: 'bone', id: 1 },
         { title: 'Selamat Datang', body: 'Lorem Ipsum', author: 'toni', id: 2 },
-        { title: 'Tips dan trik', body: 'Lorem Ipsum', author: 'komang', id: 3 }
+        { title: 'Tips dan trik', body: 'Lorem Ipsum', author: 'bone', id: 3 }
     ])
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="Semua Judul" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'bone')} title="Blog Bone" />
         </div>
     );
 }
