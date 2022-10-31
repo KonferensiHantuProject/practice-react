19a3ba6eda2e7bf15c4702776e2893d814ee7c61
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-10-31 18:29:46 +0700

    Adding Error Handling

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     // Run Every time the data change
     useEffect(() => {
@@ -12,18 +13,27 @@ const Home = () => {
             fetch('http://localhost:8222/blogs')
             // Getting response
             .then(res => {
+                if(!res.ok){
+                    throw Error('Tidak bisa mendapatkan data')
+                }
                 return res.json()
             })
             // Getting data
             .then((data) => {
                setBlogs(data);
                setIsPending(false);
+               setError(null);
+            })
+            .catch((err) => {
+                setError(err.message);
+                setIsPending(false);
             })
         }, 1000)
     }, []);
 
     return (
         <div className="home">
+            { error && <div>{ error }</div>}
             { isPending && <div> Loading ... </div>}
             {/* If blogs is not null then it will render */}
             {blogs && <BlogList blogs={blogs} title="Semua Judul" />}
