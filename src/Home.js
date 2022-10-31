c3798c35fdd3d05c5dbb0b0d9a7a361136872a7f
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-10-31 18:20:34 +0700

    Adding Loading Message

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,27 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true);
 
     // Run Every time the data change
     useEffect(() => {
-      fetch('http://localhost:8222/blogs')
-        // Getting response
-        .then(res => {
-            return res.json()
-        })
-        // Getting data
-        .then((data) => {
-           setBlogs(data);
-        })
+        setTimeout(() => {
+            fetch('http://localhost:8222/blogs')
+            // Getting response
+            .then(res => {
+                return res.json()
+            })
+            // Getting data
+            .then((data) => {
+               setBlogs(data);
+               setIsPending(false);
+            })
+        }, 1000)
     }, []);
 
     return (
         <div className="home">
+            { isPending && <div> Loading ... </div>}
             {/* If blogs is not null then it will render */}
             {blogs && <BlogList blogs={blogs} title="Semua Judul" />}
         </div>
